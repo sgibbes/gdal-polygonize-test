6c69777afe6da70af27d26ee035fc91cf5f9d2d7
Ubuntu <ubuntu@ip-172-30-2-183.ec2.internal>
2017-04-06 15:13:16 +0000

    finalized polygonize process, add combine.sh

--- polygonize_tcd.py
+++ polygonize_tcd.py
@@ -1,41 +1,42 @@
 import subprocess
+import glob
+import os
 
-tile_list = ['00N_000E']
+tile_list = ['00N_100E']
 
 for tile_name in tile_list:
 
     # download tcd tile
     extent_url = r'http://commondatastorage.googleapis.com/earthenginepartners-hansen/GFC2014/Hansen_GFC2014_treecover2000_{}.tif'.format(tile_name)
 
-    extent_local = r'/home/ubuntu/{}.tif'.format(tile_name)
+    extent_local = '{}.tif'.format(tile_name)
 
-   # subprocess.check_call(['wget', '-O', extent_local, extent_url])
+    subprocess.check_call(['wget', '-O', extent_local, extent_url])
 
     # mask tcd tile
     masked_tile_30 = '{}_masked.tif'.format(tile_name)
-    cmd = ['gdal_calc.py', '-A', extent_local, masked_tile_30, '--calc=A>30', '--NoDataValue=0', '--co', 'COMPRESS=LZW']
-
-    #subprocess.check_call(cmd)
+    cmd = ['gdal_calc.py', '-A', extent_local, '--outfile={}'.format(masked_tile_30), '--calc=A>30', '--NoDataValue=0', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
 
     # polygonize tile
     polygonize_cmd = ['./test.sh', '-m', 'parallel', masked_tile_30]
-    #subprocess.check_call(polygonize_cmd)
+    subprocess.check_call(polygonize_cmd)
 
     # merge the tile
-    merge_cmd = ['./merge.sh']
-    #subprocess.check_call(merge_cmd)
+    combine_cmd = ['./combine.sh']
+    subprocess.check_call(combine_cmd)
 
     # zip the tile
-    zipped_tile = '~/gdal-polygonize-test/{}.zip'.format(tile_name)
-    zip_cmd = ['zip', zipped_tile, 'merge*']
-    print zip_cmd
+    merge_shape_list = glob.glob('merge.*')
+    zipped_tile = '{}.zip'.format(tile_name)
+    zip_cmd = ['zip', zipped_tile] +  merge_shape_list
     subprocess.check_call(zip_cmd)
 
     # upload to s3
     upload_cmd = ['aws', 's3', 'cp', zipped_tile, 's3://gfw-files/sam/']
+    subprocess.check_call(upload_cmd)
 
     # clean directory
-    file_to_rm = ['merged*', '*.tif']
-    for file in file_to_rm:
-        rm_cmd = ['rm', 'file']
-        subprocess.check_call(rm_cmd)
+    files_to_remove = [extent_local, masked_tile_30, zipped_tile] + merge_shape_list
+    for file in files_to_remove:
+        os.remove(file)
